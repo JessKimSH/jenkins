84fbc8605bad11a0483436f7bec88c3aef61bd58
JessKimSH <official1126@naver.com>
2019-07-03 17:20:52 +0900

    Update app.js

--- app.js
+++ app.js
@@ -5,5 +5,6 @@ var server = http.createServer(function (req, res) {
   res.writeHead(200, { 'Content-Type' : 'text/plain' });
   res.end('Hello World');
 });
+console.log('Successfully Connected')
 
 server.listen(3000);
